d5cd85a9b82aa45fb7411d7179e4b6e496c0deed
Selahattin Ulaşcan Kılıç <ulascank1@gmail.com>
2019-11-16 23:13:57 +0300

    Add files via upload
    Socket io Access-Control-Allow-Origin error fix

--- ws.py
+++ ws.py
@@ -3,13 +3,14 @@ from flask import Flask, request
 from flask_socketio import SocketIO, emit
 import threading
 import socket
+import time
 
 from websocket.client import Client
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app,  ping_timeout=20, ping_interval=10)
+socketio = socketio = SocketIO(app, cors_allowed_origins="*")
 clients = {}
 
 host = None
@@ -26,33 +27,38 @@ def connected():
 
 @socketio.on('disconnect')
 def disconnect():
-    player = clients[request.sid].player
-    if player is not None:
-        player.despawn()
-        player.room.dungeon.left_player(player)
-    clients.pop(request.sid, None)
+    if has_client(request.sid):
+        player = clients[request.sid].player
+        if player is not None:
+            player.despawn()
+            player.room.dungeon.left_player(player)
+        clients.pop(request.sid, None)
 
 
 @socketio.on('speech')
 def speech(message):
-    player = clients[request.sid].player
-    if player is not None:
-        player.speech(message)
+    if has_client(request.sid):
+        player = clients[request.sid].player
+        if player is not None:
+            player.speech(message)
 
 
 @socketio.on('dungeons')
 def send_dungeon_list():
-    dungeons_data = {}
-    for i, dungeon in enumerate(dungeons):
-        dungeons_data[i] = {"label":dungeon.label, "subject":dungeon.subject}
-    emit('dungeons', dungeons_data, room=request.sid)
+    if has_client(request.sid):
+        dungeons_data = {}
+        for i, dungeon in enumerate(dungeons):
+            dungeons_data[i] = {"label":dungeon.label, "subject":dungeon.subject}
+        emit('dungeons', dungeons_data, room=request.sid)
 
 
 @socketio.on('set_user_info')
 def set_user_info(user_info):
-    if "nick" in user_info and "skin" in user_info:
-        clients[request.sid].nick = user_info["nick"]
-        clients[request.sid].skin = user_info["skin"]
+    if has_client(request.sid):
+        if "nick" in user_info and "skin" in user_info:
+            clients[request.sid].nick = user_info["nick"]
+            clients[request.sid].skin = user_info["skin"]
+
 
 @socketio.on('get_user_info')
 def get_user_info():
@@ -61,29 +67,38 @@ def get_user_info():
 
 @socketio.on('join_dungeon')
 def join_dungeon(dungeon_number):
-    if 0 <= dungeon_number < len(dungeons):
-        clients[request.sid].join_dungeon(dungeon_number)
+    if has_client(request.sid):
+        if 0 <= dungeon_number < len(dungeons):
+            clients[request.sid].join_dungeon(dungeon_number)
 
 
 @socketio.on('get_room_data')
 def load_room_data():
-    clients[request.sid].load_room_data()
+    if has_client(request.sid):
+        clients[request.sid].load_room_data()
 
 
 @socketio.on('get_characters')
 def get_characters():
-    clients[request.sid].get_characters()
+    if has_client(request.sid):
+        clients[request.sid].get_characters()
 
 
 @socketio.on('click')
 def click(data):
-    if "id" in data and "type" in data:
-        clients[request.sid].click(data["id"], data["type"])
+    if has_client(request.sid):
+        if "id" in data and "type" in data:
+            clients[request.sid].click(data["id"], data["type"])
 
 
 @socketio.on('answer')
 def answer(data):
-    clients[request.sid].answer(data)
+    if has_client(request.sid):
+        clients[request.sid].answer(data)
+
+
+def has_client(id):
+    return id in list(clients.keys())
 
 
 def websocket_thread():
