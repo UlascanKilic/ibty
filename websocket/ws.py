733c7ee0e33663249a2ab87d6dc3dfba3a5b5131
S. Ulaşcan Kılıç <ulascank1@gmail.com>
2020-07-01 15:58:08 +0300

    Update ws.py

--- websocket/ws.py
+++ websocket/ws.py
@@ -10,11 +10,11 @@ from websocket.client import Client
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app,  ping_timeout=20, ping_interval=10)
 clients = {}
 
-host = 'https://romantic-ptolemy-17ec44.netlify.app/'
-port = None
+host = None
+port = 8888
 
 dungeons = []
 
@@ -109,7 +109,11 @@ def websocket_start(_dungeons):
     global dungeons, host, port
     dungeons = _dungeons
 
-   
+    if host is None:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        host = s.getsockname()[0]
+        s.close()
 
     thr_ws = threading.Thread(target=websocket_thread, args=(), kwargs={})
     thr_ws.start()
